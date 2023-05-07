5075694300a49470e7b26a72b9d9435e18378ff8
Kattesh <kate.sh@icloud.com>
2023-05-07 15:26:20 -0500

    navigation

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,53 +1,44 @@
-import {Image, StyleSheet, Text, View} from "react-native";
+import {Image, Pressable, StyleSheet, Text, View} from "react-native";
 import {TweetType} from "../types";
 import {Entypo, EvilIcons,} from "@expo/vector-icons";
 import React from "react";
+import {IconButton} from "../components/IconButton";
+import {Link} from "expo-router";
 
 
-type IconButtonProps = {
-    icon: React.ComponentProps<typeof EvilIcons>['name'],
-    text?: number | string
-}
-const IconButton = ({icon, text}: IconButtonProps) => {
-    return (
-        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <EvilIcons name={icon} size={22} color='gray'/>
-            <Text style={{fontSize: 12, color: 'gray'}}>{text}</Text>
-        </View>
-    )
-}
 type TweetProps = {
     tweet: TweetType
 }
 
 const Tweet = ({tweet}: TweetProps) => {
     return (
-        <View style={styles.container}>
-            <Image src={tweet.user.image} style={styles.userImage}/>
+        <Link href={`/${tweet.id}`} asChild>
+            <Pressable style={styles.container}>
+                <Image src={tweet.user.image} style={styles.userImage}/>
+                <View style={styles.mainContainer}>
+                    <View style={styles.textContainer}>
+                        <Text style={styles.name}>{tweet.user.name}</Text>
+                        <Text style={styles.username}>{tweet.user.username} ·2h</Text>
+                        <Entypo
+                            name="dots-three-horizontal"
+                            size={16}
+                            color="gray"
+                            style={{marginLeft: 'auto'}}/>
+                    </View>
+                    <Text style={styles.content}>{tweet.content}</Text>
 
-            <View style={styles.mainContainer}>
-                <View style={styles.textContainer}>
-                    <Text style={styles.name}>{tweet.user.name}</Text>
-                    <Text style={styles.username}>{tweet.user.username} ·2h</Text>
-                    <Entypo
-                        name="dots-three-horizontal"
-                        size={16}
-                        color="gray"
-                        style={{marginLeft: 'auto'}}/>
+                    {tweet.image && <Image src={tweet.image} style={styles.image}/>}
+
+                    <View style={styles.footer}>
+                        <IconButton icon='comment' text={tweet.numberOfComments}/>
+                        <IconButton icon='retweet' text={tweet.numberOfRetweets}/>
+                        <IconButton icon='heart' text={tweet.numberOfLikes}/>
+                        <IconButton icon='chart' text={tweet.impressions || 0}/>
+                        <IconButton icon='share-apple'/>
+                    </View>
                 </View>
-                <Text style={styles.content}>{tweet.content}</Text>
-
-                {tweet.image && <Image src={tweet.image} style={styles.image}/>}
-
-                <View style={styles.footer}>
-                    <IconButton icon='comment' text={tweet.numberOfComments}/>
-                    <IconButton icon='retweet' text={tweet.numberOfRetweets}/>
-                    <IconButton icon='heart' text={tweet.numberOfLikes}/>
-                    <IconButton icon='chart' text={tweet.impressions || 0}/>
-                    <IconButton icon='share-apple'/>
-                </View>
-            </View>
-        </View>
+            </Pressable>
+        </Link>
     )
 }
 
